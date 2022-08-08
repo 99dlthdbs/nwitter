cb3aa86ceef8c19e5f4220eda1d42f726dd1fa22
Lee-SoYoon <99dlthdbs@gmail.com>
2022-08-08 22:49:08 +0900

    Init 🎉 firebase 설치

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyC61xxnrshLBmRzK0t1d_I4rtX1iahfRgk',
+  authDomain: 'nwitter-a776b.firebaseapp.com',
+  projectId: 'nwitter-a776b',
+  storageBucket: 'nwitter-a776b.appspot.com',
+  messagingSenderId: '537785628253',
+  appId: '1:537785628253:web:74b1a3a45cf657b0d97fb4',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
