8a985a5bc4deda25ca751b070e47d57da042a170
Lee-SoYoon <99dlthdbs@gmail.com>
2022-08-09 22:15:20 +0900

    Update ✨ Log in 기능 추가

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,7 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from 'firebase/auth';
 
 function Auth() {
-  return <div>Auth</div>;
+  const auth = getAuth();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState('');
+  const onChange = (event) => {
+    const {
+      target: { name, value },
+    } = event;
+    if (name === 'email') {
+      setEmail(value);
+    } else if (name === 'password') {
+      setPassword(value);
+    }
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      let data;
+      if (newAccount) {
+        data = await createUserWithEmailAndPassword(auth, email, password);
+      } else {
+        data = await signInWithEmailAndPassword(auth, email, password);
+      }
+      console.log(data);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+  const toggleAccount = () => {
+    setNewAccount((prev) => !prev);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          name='email'
+          type='email'
+          placeholder='Email'
+          required
+          value={email}
+        />
+        <input
+          onChange={onChange}
+          name='password'
+          type='text'
+          placeholder='Password'
+          required
+          value={password}
+        />
+        <input type='submit' value={newAccount ? 'Create Account' : 'Log In'} />
+        {error}
+      </form>
+      <span onClick={toggleAccount}>
+        {newAccount ? 'Log In' : 'Create Account'}
+      </span>
+      <button>Continue with Google</button>
+      <button>Continue with Github</button>
+    </div>
+  );
 }
 
 export default Auth;
